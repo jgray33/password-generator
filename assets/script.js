88d09370d96739d4cfcffe9726aa8ce1a1b0311b
Julia <julia.gray30@gmail.com>
2021-12-29 10:33:44 +0000

    first prompt added

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function passwordLength() {
+  let text; 
+  let person = prompt("Length of password", 
+  "At least 8 characters and no more than 124 characters");
+  if (person == null || person == "" ) {
+    text = "User cancelled the prompt";
+  } else { 
+    characterTypes()
+      }
+  }
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,7 +20,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+}document.getElementById("generate")
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
