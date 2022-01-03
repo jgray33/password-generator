2fbb0840a17dc57342b3dd433442abef767edf70
Julia <julia.gray30@gmail.com>
2022-01-03 18:11:05 +0000

    all prompts added

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 
 function passwordPrompt () {
-let passwordLength = prompt ("Length of password",
-"At least 8  characters and no more than 124 characters") 
+let passwordLength = prompt ("Length of password\n Password must be at least 8 characters and no more than 124 characters",
+"Enter a number and press ok") 
+let lowerCase = prompt ("Do you want to include lowercase?\n Type yes or no and press ok",
+"yes")
+let upperCase = prompt ("Do you want to include uppercase?\n Type yes or no and press ok", 
+"yes")
+let numericChar = prompt ("Do you want to include numbers?\n Type yes or no and press ok",
+"yes")
+let specialChar = prompt ("Do you want to include special characters?\n Type yes or no and press ok", 
+"yes")
 }
 
 
