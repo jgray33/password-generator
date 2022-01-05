e5ccc673e0744dbd101463b6129988463d628e72
Julia <julia.gray30@gmail.com>
2022-01-05 13:39:16 +0000

    arrays added

--- assets/script.js
+++ assets/script.js
@@ -46,13 +46,15 @@ if (upperCase == false &&
 // 3. Generate password
 
 let upperCaseChar = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
-let lowerCaseCar = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,Z]
-let specialCharList = [!,",#,$,%,&,',(,),*,+,,,-,.,/,:,;,<,=,>,?,@,[,,^,_,`,{,|,},~]
-let numbers =
+let lowerCaseCar = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+let specialCharList = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[",","^",_","`","{","|","}","~"]
+let numericalList = [1,2,3,4,5,6,7,8,9,0]
+
 
 // 4. Return password
     return "Password"
 }
+
 
 
 // Write password to the #password input
